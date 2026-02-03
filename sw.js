eca2926cf82f2aa223d836cc401f642b0e0ebd4c
KingAmada <sarkiamada@gmail.com>
2026-02-03 04:15:15 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'wedding-card-v1';
+const ASSETS_TO_CACHE = [
+  './wedding_card.html',
+  './manifest.json',
+  // If you have local images, list them here, e.g., './my-image.jpg'
+];
+
+// Install Event: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch Event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate Event: Cleanup old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
